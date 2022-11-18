9294a82bc7289952f14337f00f246d7327695dd7
Jassyra <jespinozaq@goldenpalace.com.pe>
2022-11-18 16:01:45 -0500

    v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {SignalrService} from "./signalr.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements  OnInit, OnDestroy{
   title = 'FRONT_SignalR';
+
+  constructor(
+    public signalrService: SignalrService
+  ) {
+  }
+  ngOnInit(): void {
+    this.signalrService.startConnection();
+
+    setTimeout(() => {
+      this.signalrService.askServerListener();
+      this.signalrService.askServer();
+    }, 2000)
+  }
+
+  ngOnDestroy(): void {
+    this.signalrService.hubConnection.off("askServerResponse")
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToastrModule } from "ngx-toastr";
+import { FormsModule } from "@angular/forms";
+import { HomeComponent } from './home/home.component';
+import { AuthComponent } from './auth/auth.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AuthComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ToastrModule.forRoot({
+      enableHtml: true,
+      timeOut: 10000,
+      positionClass: 'toast-top-right',
+      preventDuplicates: false
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
